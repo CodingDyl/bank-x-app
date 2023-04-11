5c6b09b1b775da9d184529aae1ee5f4c56038e0d
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-04-11 11:24:13 +0200

    starting project

---
+++ src/main/java/za/co/entelect/demo/BankXAppApplication.java
@@ -0,0 +1,13 @@
+package za.co.entelect.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankXAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankXAppApplication.class, args);
+	}
+
+}
